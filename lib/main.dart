36c6a730f99c017370f96f735aaf9e1584b91e8b
AlamNur99 <muh.alamnur.haris99@gmail.com>
2025-02-14 21:07:36 +0800

    build web

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Happy Valentine Uggaa',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
